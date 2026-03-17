4243cb70d64b577fdf2f8c9aa6619e757bc61511
Radovan Varchol <rado.varchol@gmail.com>
2026-03-17 11:39:00 -0400

    Updated emotion_detection.py

--- emotion_detection.py
+++ emotion_detection.py
@@ -6,15 +6,11 @@ def emotion_detector(text_to_analyze: str):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     input_json = { "raw_document": { "text": text_to_analyze } }
-    
     response = requests.post( url = url, headers = headers, json = input_json )
-    
     return response
 
 
 if __name__ == "__main__":
     text = "I feel sad today."
-
     response = emotion_detector(text)
-
     print(response)
