c2f58d12f1a498ae5a78c6a8b8b6b16a54dcf22a
Radovan Varchol <rado.varchol@gmail.com>
2026-03-17 11:49:28 -0400

    Modify emotion_detection.py

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,14 +1,33 @@
 import requests
 import json
 
-
 def emotion_detector(text_to_analyze: str):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     input_json = { "raw_document": { "text": text_to_analyze } }
-    response = requests.post( url = url, headers = headers, json = input_json )
-    return response
 
+    response = requests.post( url = url, headers = headers, json = input_json )
+
+    response = emotion_detector(text)
+
+        # Handling for empty request
+    if response.status_code == 400:
+        empty_dict = {
+            "joy": None,
+            "anger" : None,
+            "disgust" : None,
+            "sadness" : None,
+            "fear"  : None,
+            "dominant_emotion": None
+        }
+        return empty_dict
+
+    response_text = response.text
+    formatted_response = json.loads(response_text)
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+    emotions['dominant_emotion'] = max(emotions, key=emotions.get)
+
+    return emotions
 
 if __name__ == "__main__":
     text = "I feel sad today."
