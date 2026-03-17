25eabd5027789e92d71b18bb44750255f355abcc
Radovan Varchol <rado.varchol@gmail.com>
2026-03-17 12:55:17 -0400

    updated emotion_deteection.py

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,35 +1,42 @@
 import requests
 import json
+from pprint import pprint
 
-def emotion_detector(text_to_analyze: str):
+def emotion_detector(text_to_analyze: str) -> dict:
+    # Connection variables
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     input_json = { "raw_document": { "text": text_to_analyze } }
-
+    
+    # Request
     response = requests.post( url = url, headers = headers, json = input_json )
+    
+    response_json = json.loads(response.text)
+    response_dict = response_json['emotionPredictions'][0]['emotion']
 
-    response = emotion_detector(text)
+    return response.text
+    max_score = {
+        'emotion_name': '',
+        'emotion_score': 0
+    }
 
-        # Handling for empty request
-    if response.status_code == 400:
-        empty_dict = {
-            "joy": None,
-            "anger" : None,
-            "disgust" : None,
-            "sadness" : None,
-            "fear"  : None,
-            "dominant_emotion": None
-        }
-        return empty_dict
+    for key, value in response_dict:
+        if value > max_score['emotion_score']:
+            max_score['emotion_score'] = value
+            max_score['emotion_name'] = key
 
-    response_text = response.text
-    formatted_response = json.loads(response_text)
-    emotions = formatted_response['emotionPredictions'][0]['emotion']
-    emotions['dominant_emotion'] = max(emotions, key=emotions.get)
+    response_obj = {
+        'anger': response['anger'],
+        'disgust': response['disgust'],
+        'fear': response['fear'],
+        'joy': response['joy'],
+        'sadness': response['sadness'],
+        'dominant_emotion': max_score['emotion_name']
+    }
 
-    return emotions
+    return response_obj
 
 if __name__ == "__main__":
-    text = "I feel sad today."
+    text = "I love this new technology."
     response = emotion_detector(text)
     print(response)
