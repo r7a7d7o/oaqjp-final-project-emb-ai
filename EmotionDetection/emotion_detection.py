65ed7a8dfd255db97fe557acdc5278ca9110cc94
r7a7d7o <rado.varchol@gmail.com>
2026-03-23 08:51:15 -0400

    modified url

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -32,8 +32,8 @@ def emotion_detector(text_to_analyze: str) -> dict:
     """
 
     # Connection variables
-    url = 'https://sn-watson-emotion.labs.skills.network/v1/ \
-    watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/'
+    url += 'watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     input_json = { "raw_document": { "text": text_to_analyze } }
 
